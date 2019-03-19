56eaa573add5b412510215ce45ccbff4af79f9fa
brunoleao92 <47321290+brunoleao92@users.noreply.github.com>
2019-03-19 09:47:40 -0300

    Add files via upload

---
+++ Clase2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char palabra[15];
+
+    printf("Ingrese una palabra: ");
+    scanf("%s", palabra);
+
+    printf("Ud. ingreso: %s" , palabra);
+
+    /* int n1;
+    int n2;
+    int suma;
+    float promedio;
+
+
+        printf("\nIngrese un numero: ");
+        scanf("%d" , &n1);
+
+        printf("Ingrese el segundo numero: ");
+        scanf("%d" , &n2);
+
+        suma=n1 + n2;
+        printf("La suma de los numeros es: %d" , suma);
+
+        promedio=(float) suma/2.;
+        printf("\nEl promedio es: %.2f" , promedio);*/
+
+
+    return 0;
+}
